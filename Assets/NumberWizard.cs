5729ad8416fcaea836116040c7691d508ca8024b
Rafał Czerniawski <rczerniawski@szpitalmswjg.local>
2018-10-19 08:50:28 +0200

    Final version

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
-    int min = 1;
-    int max = 1000;
-    int guess = 500;
-    int guessnumber = 0;
+    int min;
+    int max;
+    int guess;
+    int guesscount;
 
     void Start()
     {
+        StartGame();
+	}
+
+    void StartGame()
+    {
+        guess = 500;
+        min = 1;
+        max = 1000;
+        guesscount = 0;
         Debug.Log("Welcome!");
         Debug.Log("Pick number between: " + min + " and " + max);
         Debug.Log("Make sure u keep that number in secret.");
@@ -18,29 +27,31 @@ public class NumberWizard : MonoBehaviour {
         Debug.Log("Use Up Arrow and Down Arrow to tell if its higher or lower.");
         Debug.Log("Press Enter if number is correct.");
         max = max + 1;
-	}
+    }
 
     void Update()
     {
 		if(Input.GetKeyDown(KeyCode.UpArrow))
         {
             min = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is it higher or lower than..." + guess);
-            Debug.Log("Actual guess range varies between:" + min + " and " + max);
-            guessnumber++;
+            NextGuess();
         }
         else if(Input.GetKeyDown(KeyCode.DownArrow))
         {
             max = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is it higher or lower than..." + guess);
-            Debug.Log("Actual guess range varies between:" + min + " and " + max);
-            guessnumber++;
+            NextGuess();
         }
         else if(Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("I guessed it in: " + guessnumber + " guesses!");
+            Debug.Log("I guessed it in: " + guesscount + " guesses!");
+            StartGame();
         }
+    }
+    
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is it higher or lower than..." + guess);
+        guesscount++;
     }
 }
